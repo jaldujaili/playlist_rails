a2e2e58d9824ab0286efb46960c1a4691e62145f
Jordan Aldujaili <jordan415@yahoo.com>
2015-04-20 22:54:28 -0700

    add auth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,34 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   # private
   # def authenticate
   #   authenticate_or_request_with_http_token do |token, options|
   #     @user = User.where(api_key: token).first
   #   end
-#   def current_user
-#     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-#   end
+
+  def auth_token
+    puts request.headers["HTTP_AUTHORIZATION"].inspect
+    return nil if request.headers["HTTP_AUTHORIZATION"].blank?
+    request.headers["HTTP_AUTHORIZATION"].gsub("Token token=\"", "").gsub("\"", "")
+  end
+  def require_api_auth
+    auth_token == "tr34tm4t3"
+  end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    # return if Rails.env.development?
+    @current_user = User.token_authenticate!(auth_token)
+    if @current_user.blank?
+      render json: {error: {message: "Failed to authenticate user"}}, status: 401
+    end
+    true
+  end
 #   helper_method :current_user
 #
 #   def authorize
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421002416) do
+ActiveRecord::Schema.define(version: 20150421044923) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -73,8 +73,10 @@ ActiveRecord::Schema.define(version: 20150421002416) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "auth_token",             default: ""
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["name"], name: "index_users_on_name"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
